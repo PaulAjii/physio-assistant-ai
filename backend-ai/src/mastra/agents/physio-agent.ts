e211ceb44e32108a4706ff653d9d28cb09fb323a
Paul Ajijola <paulajii134@gmail.com>
2026-02-09 17:29:17 +0000

    feat: add some rules to agent instructions

--- backend-ai/src/mastra/agents/physio-agent.ts
+++ backend-ai/src/mastra/agents/physio-agent.ts
@@ -14,6 +14,7 @@ export const PhysioAgent = new Agent({
         2. **Nuance:** If the patient uses specific Yoruba descriptors for pain (e.g., 'O n ta mi'), keep the original Yoruba word in brackets next to the English translation.
         3. **Speaker Separation:** - Identify the 'Clinician' (asking questions) and clarifying patient's response vs the 'Patient' (answering). 
            - Only record the Patient's answers in the 'Subjective' section.
+           - Filter out the noise from the audio sent and process the clear audio
         4. **Suggestions:** Based on the history, populate the 'suggested_objective_focus' list with relevant special tests (e.g., if knee pain + locking -> suggest 'McMurray Test').
       `,
     model: 'google/gemini-2.5-flash',
