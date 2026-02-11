47fd0d83bc2cd3100afbc9a369db52d7f935eb78
PaulAjii <paulajii134@gmail.com>
2026-02-11 15:48:37 +0100

    feat: handle filename addition to the service

--- backend-ai/src/medical-agent/medical-agent.controller.ts
+++ backend-ai/src/medical-agent/medical-agent.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  HttpStatus,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { MedicalAgentService } from './medical-agent.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import type { Express } from 'express';
@@ -8,19 +15,32 @@ export class MedicalAgentController {
   constructor(private readonly medicalAgentService: MedicalAgentService) {}
 
   @Post('/process-audio')
-  @UseInterceptors(FileInterceptor('file', {
-    limits: { fileSize: 50 * 1024 * 1024 }
-  }))
-  async processAudio(@UploadedFile(new ParseFilePipeBuilder().addMaxSizeValidator({
-    maxSize: 50 * 1024 * 1024
-  }).build({
-    errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
-  })) file: Express.Multer.File) {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: { fileSize: 50 * 1024 * 1024 },
+    }),
+  )
+  async processAudio(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addMaxSizeValidator({
+          maxSize: 50 * 1024 * 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
     if (!file) {
       return { error: 'No file uploaded' };
     }
 
-    const result = await this.medicalAgentService.processConsultation(file.buffer, file.mimetype);
+    const result = await this.medicalAgentService.processConsultation(
+      file.buffer,
+      file.mimetype,
+      file.originalname,
+    );
     return { message: 'Audio processed successfully', data: result };
   }
 }
