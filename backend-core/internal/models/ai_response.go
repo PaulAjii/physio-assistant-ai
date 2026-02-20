5ee99ef9d18131c27536fc127139f37c61766c77
PaulAjii <paulajii134@gmail.com>
2026-02-20 23:04:49 +0100

    feat: create ai response schema

---
+++ backend-core/internal/models/ai_response.go
@@ -0,0 +1,39 @@
+package models
+
+type AIResponse struct {
+	Message string `json:"message"`
+	Data    AIData `json:"data"`
+}
+
+type AIData struct {
+	Meta                    Meta       `json:"meta"`
+	Subjective              Subjective `json:"subjective"`
+	SuggestedObjectiveFocus []string   `json:"suggested_objective_focus"`
+}
+
+type Meta struct {
+	LanguageDetected      string `json:"language_detected"`
+	SpeakerIdentification string `json:"speaker_identification"`
+}
+
+type Subjective struct {
+	PresentingComplaint    string      `json:"presenting_complaint"`
+	HistoryOfComplaint     string      `json:"history_of_complaint"`
+	PainProfile            PainProfile `json:"pain_profile"`
+	RedFlags               []string    `json:"red_flags"`
+	AssociatedSymptoms     []string    `json:"associated_symptoms"`
+	RelevantMedicalHistory []string    `json:"relevant_medical_history"`
+	DrugHistory            []string    `json:"drug_history"`
+	PastSurgicalHistory    []string    `json:"past_surgical_history"`
+	FamilyHistory          string      `json:"family_history"`
+	SocialHistory          []string    `json:"social_history"`
+}
+
+type PainProfile struct {
+	Intensity   int      `json:"intensity"`
+	Quality     string   `json:"quality"`
+	Aggravating []string `json:"aggravating"`
+	Alleviating []string `json:"alleviating"`
+	Duration    string   `json:"duration"`
+	Location    []string `json:"location"`
+}
