2f3ce69731321ef607c11ffff7cabbed28eeb47e
PaulAjii <paulajii134@gmail.com>
2026-02-24 01:38:01 +0100

    feat: create consultation composable to handle api logic for frontend

---
+++ frontend/app/composables/useConsultation.ts
@@ -0,0 +1,45 @@
+// API Logic
+export const useConsultation = () => {
+  const config = useRuntimeConfig();
+  const baseUri = config.public.apiBaseUri;
+
+  const uploadAudio = async (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await $fetch<{
+      status: string;
+      message: string;
+      statusCode: number;
+      jobId?: string;
+    }>(`${baseUri}/consultation/upload`, {
+      method: "POST",
+      body: formData,
+    });
+    return response;
+  };
+
+  const streamResult = async (
+    jobId: string,
+    onResult: (data: any) => void,
+    onError: (error: any) => void,
+  ) => {
+    const eventSource = new EventSource(
+      `${baseUri}/consultation/stream/${jobId}`,
+    );
+
+    eventSource.addEventListener("result", (e) => {
+      const data = JSON.parse(e.data);
+      onResult(data);
+      onError(null);
+    });
+
+    eventSource.addEventListener("error", (e) => {
+      const data = JSON.parse((e as MessageEvent).data);
+      onError(data);
+      eventSource.close();
+    });
+    return eventSource;
+  };
+  return { uploadAudio, streamResult };
+};
