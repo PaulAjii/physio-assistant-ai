01ff7664420c3fd454d5a5185ce81b6f31ef4274
PaulAjii <paulajii134@gmail.com>
2026-02-27 15:11:25 +0100

    fix: correct casing of jobId to jobID in AssessmentSubmission interface

--- frontend/types/assessment.d.ts
+++ frontend/types/assessment.d.ts
@@ -64,7 +64,7 @@ export interface CollatedAssessment {
 }
 
 export interface AssessmentSubmission {
-  jobId: string;
+  jobID: string;
   complaint: string;
   corrected_subjective: Subjective;
   objective_findings: ObjectiveFinding[];
